11161fd2c7c14d8219f41d8338b6d3d09f6715f6
lawrence1130 <samlawrencedadang@gmail.com>
2025-09-15 22:16:28 +0800

    added main menu function

--- factorial.js
+++ factorial.js
@@ -5,3 +5,26 @@ const rl = ealine.createInterface({
  input: process.stdin,
  output: process.stdin
 })
+
+function mainMenu(){
+    console.log("------------------------");
+    console.log("Factorial Application");
+    console.log("1. Say Hello");
+    console.log("2.Factorial");
+    console.log("3. Exit Application");
+    console.log("------------------------");
+    
+    rl.question("Enter your choice(1-3): ", choice =>{
+
+        if(choice === "1"){
+            sayHello();
+        } else if (choice === "2"){
+            computerFactorial();
+        } else if (choice === "3"){
+            exitProgram();
+        } else {
+            console.log("Invalid Choice. Please Try Again");
+        }
+    })
+}
+
