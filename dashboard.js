4ecf8446e217fee3a0c800af59f373d0167262ca
lawrence1130 <samlawrencedadang@gmail.com>
2025-10-28 08:02:52 +0800

    added radarchart from radarchart.html script

--- dashboard.js
+++ dashboard.js
@@ -56,3 +56,67 @@ class LineChart{
         chart.init();
     });
 
+class radarChart{
+        constructor(canvasId,dataUrl){
+            this.canvasId = canvasId;
+            this.dataUrl = dataUrl;
+            this.chart = null;
+        }
+
+        renderChart(data){
+            const ctx = document.getElementById(this.canvasId).getContext("2d");
+
+            this.chart = new Chart(ctx,{
+                type:"radar",
+                data:{
+                    //Labels
+                    labels:data.labels,
+                    //Datasets
+                    datasets: data.datasets
+                },
+                options:{
+                    responsive: true,
+                    plugins:{
+                        legend:{
+                           position:"top"
+                        },
+                        title:{
+                            display: true,
+                            //Title
+                            text:"Radar Chart"
+                        }
+                    },
+                    scales:{
+                        r:{
+                            angleLines:{display:true},
+                            suggestedMin:0,
+                            suggestedMax:100
+                        }
+                    }
+                }
+            });
+        }
+
+        async fetchData(){
+            try{
+                const response = await fetch(this.dataUrl);
+                if(!response.ok) throw new Error(`Failed to load data: ${response.statusText}`);
+
+                const data = await response.json();
+                return data;
+            } catch(error){
+                console.error("Error fetching data: ", error);
+                return null;
+            }
+        }
+
+        async init(){
+            const data = await this.fetchData();
+            if(data) this.renderChart(data);
+        }
+    }
+
+    document.addEventListener("DOMContentLoaded", ()=>{
+        const chart = new radarChart("radarChart","radarChart.json");
+        chart.init();
+    });
